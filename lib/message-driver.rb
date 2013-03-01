332ddbf1fa2bd32cc0285de23eefa997cca20c03
Matt Campbell <matt@soupmatt.com>
2013-03-01 14:15:13 -0600

    initial commit

---
+++ lib/message-driver.rb
@@ -0,0 +1,7 @@
+require "message-driver/version"
+
+module Message
+  module Driver
+    # Your code goes here...
+  end
+end
---
+++ lib/message-driver/version.rb
@@ -0,0 +1,5 @@
+module Message
+  module Driver
+    VERSION = "0.0.1"
+  end
+end
