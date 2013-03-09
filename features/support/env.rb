278d137ae29ca37384a07a147525a71deadf0d44
Matt Campbell <matt@soupmatt.com>
2013-03-09 17:14:46 -0600

    more broker config refactor

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,23 +1,9 @@
+require File.join(File.dirname(__FILE__), '..', '..', 'test_lib', 'broker_config')
+
 require 'message_driver'
 
-def _broker_config
-  adapter_file = File.expand_path("../../../.adapter_under_test", __FILE__)
-  adapter = ENV['ADAPTER'] || (File.exist?(adapter_file) && File.read(adapter_file).chomp)
-  case adapter
-  when 'bunny'
-    {
-      adapter: :bunny,
-      vhost: 'message-driver-test'
-    }
-  when 'in_memory'
-    {adapter: :in_memory}
-  else
-    {adapter: :in_memory}
-  end
-end
-
 Before do
-  MessageDriver.configure(_broker_config)
+  MessageDriver.configure(BrokerConfig.config)
 end
 
 After do
