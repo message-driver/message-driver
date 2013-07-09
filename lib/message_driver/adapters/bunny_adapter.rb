98e3c55c8b5ce75fccd31d2b48c4e74cced34dd0
Matt Campbell <matt@soupmatt.com>
2013-07-09 10:59:56 -0500

    require bunny ~> 0.9.0.rc2

--- lib/message_driver/adapters/bunny_adapter.rb
+++ lib/message_driver/adapters/bunny_adapter.rb
@@ -260,10 +260,10 @@ module MessageDriver
       private
 
       def validate_bunny_version
-        required = Gem::Requirement.create('~> 0.9.0.pre7')
+        required = Gem::Requirement.create('~> 0.9.0.rc2')
         current = Gem::Version.create(Bunny::VERSION)
         unless required.satisfied_by? current
-          raise MessageDriver::Error, "bunny 0.9.0.pre7 or later is required for the bunny adapter"
+          raise MessageDriver::Error, "bunny 0.9.0.rc2 or later is required for the bunny adapter"
         end
       end
     end
