cada67baa42170c0844f5eb45b070c03b28547fd
Matt Campbell <matt@soupmatt.com>
2013-07-24 09:44:20 -0500

    Wait even longer on travis

--- spec/support/utils.rb
+++ spec/support/utils.rb
@@ -1,8 +1,9 @@
 module Utils
   def pause_if_needed(seconds=0.1)
-    seconds *= 10 if ENV['TRAVIS'] == 'true'
+    seconds *= 20 if ENV['TRAVIS'] == 'true'
     case BrokerConfig.current_adapter
     when :bunny
+      puts "sleeping for #{seconds} seconds"
       sleep seconds
     end
   end
