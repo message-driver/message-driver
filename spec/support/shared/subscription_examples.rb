ad0215fc766cdc0385827fe59a2270909044bb8e
Matt Campbell <matt@soupmatt.com>
2013-07-24 10:21:26 -0500

    Exclude a few more tests on travis

--- spec/support/shared/subscription_examples.rb
+++ spec/support/shared/subscription_examples.rb
@@ -85,7 +85,7 @@ shared_examples "subscriptions are supported" do |subscription_type|
         expect(bodies).to include(message2)
       end
 
-      it "removes the messages from the queue" do
+      it "removes the messages from the queue", :no_travis do
         expect {
           subscription
           pause_if_needed
@@ -122,7 +122,7 @@ shared_examples "subscriptions are supported" do |subscription_type|
         destination.publish(message2)
       end
 
-      it "keeps processing the messages" do
+      it "keeps processing the messages", :no_travis do
         expect {
           subscription
           pause_if_needed
